8683b2b6cc0e4a812839001ed88e041bd57bf4c4
Alain DEMENET <ademenet@e2r7p11.42.fr>
2016-04-29 15:00:04 +0200

    ft_putwchar display wchar_t

---
+++ utils/ft_putwchar.c
@@ -0,0 +1,65 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putwchar.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ademenet <ademenet@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/04/29 11:20:08 by ademenet          #+#    #+#             */
+/*   Updated: 2016/04/29 14:58:26 by ademenet         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+/*
+** These are unicode masks:
+** 0xxxxxxx // 0x00
+** 110xxxxx 10xxxxxx // 0xC0 0x80
+** 1110xxxx 10xxxxxx 10xxxxxx // 0xE0 0x80 0x80
+** 11110xxx 10xxxxxx 10xxxxxx 10xxxxxx // 0xF0 0x80 0x80 0x80
+*/
+
+int			ft_wchar_len(wchar_t wchar)
+{
+	if (wchar <= 0x7f)
+		return (1);
+	else if (wchar <= 0x7ff)
+		return (2);
+	else if (wchar <= 0xffff)
+		return (3);
+	else
+		return (4);
+}
+
+/*
+** This function takes a wchar_t and writes it. It starts with big endian
+** and move to lower endians thanks to masks and bit operations.
+*/
+
+void		ft_putwchar(wchar_t wchar)
+{
+	int		size;
+
+	size = ft_wchar_len(wchar);
+	if (size == 1)
+		ft_putchar(wchar);
+	else if (size == 2)
+	{
+		ft_putchar((wchar >> 6) + 0xC0); // Je deplace de 6 sur la droite et j'ajoute 11000000
+		ft_putchar((wchar & 0x3F) + 0x80); // Je filtre par 111111 et j'ajoute 10000000
+	}
+	else if (size == 3)
+	{
+		ft_putchar((wchar >> 12) + 0xE0); // Je deplace de 12 sur la droite et j'ajoute 11100000
+		ft_putchar(((wchar >> 6) & 0x3F) + 0x80); // Je deplace de 6, je filtre par 111111 et j'ajoute 10000000
+		ft_putchar((wchar & 0x3F) + 0x80); // Je filtre par 111111 et j'ajoute 10000000
+	}
+	else
+	{
+		ft_putchar((wchar >> 18) + 0xF0); // Je deplace de 18 sur la droite et j'ajoute 11110000
+		ft_putchar(((wchar >> 12) & 0x3F) + 0x80); // Je deplace de 12 sur la droite, je filtre par 111111 et j'ajoute 10000000
+		ft_putchar(((wchar >> 6) & 0x3F) + 0x80); // Je deplace de 6 sur la droite, je filtre par 111111 et j'ajoute 10000000
+		ft_putchar((wchar & 0x3F) + 0x80); // Je filtre par 111111 et j'ajoute 10000000
+	}
+}
