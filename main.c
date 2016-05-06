f28ccfd4063903c1812e5cc6090cb795bf977c88
Alain DEMENET <ademenet@e2r8p12.42.fr>
2016-05-06 17:16:13 +0200

    Save

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: ademenet <ademenet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/04/19 14:18:08 by ademenet          #+#    #+#             */
-/*   Updated: 2016/05/06 15:40:07 by ademenet         ###   ########.fr       */
+/*   Updated: 2016/05/06 17:13:24 by ademenet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,9 @@ int		main()
 	int ret1;
 	int ret2;
 
-	ret1 = printf("{%7p}", 0);
+	ret1 = printf("{%-20p}", &ret1);
 	puts("");
-	ret2 = ft_printf("{%7p}", 0);
+	ret2 = ft_printf("{%-20p}", &ret1);
 
 	printf("ret1 = %d, ret2 = %d", ret1, ret2);
 	return (0);
