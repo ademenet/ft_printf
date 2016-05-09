17b4b9ba3de1fd477ea0d83555c2aaf2d35a95d6
Alain DEMENET <ademenet@e2r8p12.42.fr>
2016-05-09 11:36:16 +0200

    Save - working on wide char and strings

--- src/ft_flag_s.c
+++ src/ft_flag_s.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_flag_s.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tvisenti <tvisenti@student.42.fr>          +#+  +:+       +#+        */
+/*   By: ademenet <ademenet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/04/22 18:20:57 by ademenet          #+#    #+#             */
-/*   Updated: 2016/05/06 11:16:05 by tvisenti         ###   ########.fr       */
+/*   Updated: 2016/05/09 11:34:16 by ademenet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,19 @@
 int			ft_handler_s(t_flag *f, va_list *ap)
 {
 	static int	mask_s[13] = {0, 0, 2, 1, 1, 2, 2, 2, 2, 2, 1, 2, 2};
+	wchar_t		*S;
 
 	ft_apply_mask(f, mask_s);
 	if (f->fla[10] == 1)
+	{
+		S = va_arg(*ap, wchar_t*);
+		while (S != '\0')
+		{
+			ft_putwchar(*S, f);
+			S++;
+		}
 		return (0);
+	}
 	else
 		f->arg = va_arg(*ap, char*);
 	if (f->arg != 0)
