a61a71d533d45318e80e623d381c4958470b030c
Alain DEMENET <ademenet@e2r8p12.42.fr>
2016-05-09 11:12:32 +0200

    Adding C support

--- src/ft_flag_C.c
+++ src/ft_flag_C.c
@@ -6,7 +6,7 @@
 /*   By: ademenet <ademenet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/05/03 12:46:22 by ademenet          #+#    #+#             */
-/*   Updated: 2016/05/03 18:05:30 by ademenet         ###   ########.fr       */
+/*   Updated: 2016/05/09 11:12:05 by ademenet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,10 @@
 
 int			ft_handler_C(t_flag *f, va_list *ap)
 {
+	static int	mask_c[13] = {0, 0, 2, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2};
+	wchar_t		C;
+
+	C = va_arg(*ap, wchar_t);
+	ft_putwchar(C, f);
 	return (0);
 }
