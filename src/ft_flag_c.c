d6b1eec5ad217245af91b058eba1c27e1d0e985d
Alain DEMENET <ademenet@e2r8p12.42.fr>
2016-05-09 11:16:51 +0200

    Adding lc support

--- src/ft_flag_c.c
+++ src/ft_flag_c.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_flag_c.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tvisenti <tvisenti@student.42.fr>          +#+  +:+       +#+        */
+/*   By: ademenet <ademenet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/04/27 15:34:28 by ademenet          #+#    #+#             */
-/*   Updated: 2016/05/06 15:46:10 by tvisenti         ###   ########.fr       */
+/*   Updated: 2016/05/09 11:14:30 by ademenet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,24 @@ int				ft_handler_c(t_flag *f, va_list *ap)
 {
 	static int	mask_c[13] = {0, 0, 2, 1, 1, 2, 2, 2, 2, 2, 1, 2, 2};
 	char		c;
+	wchar_t		C;
 	char		s[2];
 
 	ft_apply_mask(f, mask_c);
-	c = (char)va_arg(*ap, int);
-	s[0] = c;
-	s[1] = '\0';
-	f->arg = s;
-	ft_handler(f);
+	if (f->fla[10] == 1)
+	{
+		C = va_arg(*ap, wchar_t);
+		ft_putwchar(C, f);
+		return (0);
+	}
+	else
+	{
+		c = (char)va_arg(*ap, int);
+		s[0] = c;
+		s[1] = '\0';
+		f->arg = s;
+		ft_handler(f);
+	}
 	if (f->arg[0] == 0)
 		ft_putchar('\0', f);
 	return (0);
