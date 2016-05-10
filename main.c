e32f1880fbc879ec61b360b4cd87d3c0269e54cd
Visentin <tvisenti@42.student.fr>
2016-05-10 18:01:42 +0200

    merge

--- main.c
+++ main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ademenet <ademenet@student.42.fr>          +#+  +:+       +#+        */
+/*   By: tvisenti <tvisenti@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/04/19 14:18:08 by ademenet          #+#    #+#             */
-/*   Updated: 2016/05/09 18:44:06 by ademenet         ###   ########.fr       */
+/*   Updated: 2016/05/10 15:06:16 by tvisenti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,15 +20,21 @@ int		main()
 	int ret2;
 	int	ret;
 
-	printf("TRUE :\n");
-	ret = printf("{%05.s}{%04.9s}{%05s}{%5.3d}{%-5.3d}", 0, NULL, "1234", 12, 12);
-	puts("");
-	printf("ret : |%d|\n", ret);
+	// printf("TRUE :\n");
+	// ret = printf("{%05.s}{%04.9s}{%05s}{%5.3d}{%-5.3d}", 0, NULL, "1234", 12, 12);
+	// puts("");
+	// printf("ret : |%d|\n", ret);
+	//
+	// printf("\nMINE :\n");
+	// ret1 = ft_printf("{%05.s}{%s}", 0, NULL);
+	// puts("");
+	// printf("ret1 : |%d|\n", ret1);
 
-	printf("\nMINE :\n");
-	ret1 = ft_printf("{%05.s}{%s}", 0, NULL);
-	puts("");
-	printf("ret1 : |%d|\n", ret1);
+	ft_printf("|%6.c|\n", 'a');
+	printf("|%6.c|\n\n", 'a');
+
+	// ft_printf("{%05.s}\n", 0);
+	// printf("{%05.s}",0);
 
 	return (0);
 }
