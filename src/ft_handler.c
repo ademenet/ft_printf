c09d1bd8935adf5e07dd88820948936b587ac757
Visentin <tvisenti@42.student.fr>
2016-05-11 11:41:18 +0200

    Remove useless lines

--- src/ft_handler.c
+++ src/ft_handler.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_handler.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ademenet <ademenet@student.42.fr>          +#+  +:+       +#+        */
+/*   By: tvisenti <tvisenti@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/05/03 18:49:48 by ademenet          #+#    #+#             */
-/*   Updated: 2016/05/10 18:29:50 by ademenet         ###   ########.fr       */
+/*   Updated: 2016/05/11 11:41:00 by tvisenti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,27 +26,22 @@ void		ft_apply_mask(t_flag *f, int *mask)
 
 int			ft_handler_char(t_flag *f)
 {
-	int	k;
-
-	k = -1;
 	if (f->fla[0] != 0)
 		ft_precision_string(f);
 	if (f->fla[1] > 0)
 		ft_width_char(f);
 	if (f->fla[1] == 0)
 		ft_display_arg(f);
-	// while (f->arg[++k] != '\0' && f->fla[1] == 0)
-	// 	ft_putchar(f->arg[k], f);
 	return (1);
 }
 
 int			ft_handler_numb(t_flag *f)
 {
-	int	k;
+	int	i;
 
-	k = -1;
+	i = -1;
 	if (f->fla[0] == -1 && f->arg[0] == '0' && !(f->fla[2] == 1 &&
-		(f->spe == 'o' || f->spe == 'O' || f->spe == 'p'))) // a revoir
+		(f->spe == 'o' || f->spe == 'O' || f->spe == 'p')))
 		f->arg = "\0";
 	if (f->fla[1] > 0)
 		ft_width_numb(f);
@@ -54,7 +49,7 @@ int			ft_handler_numb(t_flag *f)
 		ft_precision_without_width(f);
 	if (f->fla[0] <= 0 && f->fla[1] == 0)
 		ft_space_plus_sharp(f);
-	while (f->arg[++k] != '\0' && f->fla[1] == 0)
-		ft_putchar(f->arg[k], f);
+	while (f->arg[++i] != '\0' && f->fla[1] == 0)
+		ft_putchar(f->arg[i], f);
 	return (1);
 }
