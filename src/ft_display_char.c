5cc892efd681a3411591906d7e225a1d38178642
Visentin <tvisenti@42.student.fr>
2016-05-11 15:45:34 +0200

    N'afficher pas les 0 si le 0 etait active et que la precision == -1, pb resolu

--- src/ft_display_char.c
+++ src/ft_display_char.c
@@ -6,7 +6,7 @@
 /*   By: tvisenti <tvisenti@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/04/30 15:41:52 by tvisenti          #+#    #+#             */
-/*   Updated: 2016/05/11 15:30:02 by tvisenti         ###   ########.fr       */
+/*   Updated: 2016/05/11 15:43:47 by tvisenti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -62,7 +62,7 @@ void		ft_zero_char(t_flag *f)
 		nb_space--;
 	while (nb_space-- > 0)
 	{
-		if (f->fla[3] == 1 && f->fla[0] == 0)
+		if (f->fla[3] == 1 && f->fla[0] <= 0)
 			ft_putchar('0', f);
 		else
 			ft_putchar(' ', f);
